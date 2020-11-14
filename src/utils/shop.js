d385c0a5cf8494a29a4eb69535bfe7f0dead5dac
Obada Darwish <obada_567@hotmail.co.uk>
2020-11-14 19:43:45 +0200

    bugfix: fix order summary total

--- src/utils/shop.js
+++ src/utils/shop.js
@@ -2,9 +2,8 @@ export const getTotal = (items) => {
     let total = 0;
     items.forEach((item) => {
         total +=
-            item.category === 'luxuryBox'
-                ? item.total
-                : item.price * item.count;
+            (item.category === 'luxuryBox' ? item.total : item.price) *
+            item.count;
     });
     return total;
 };
