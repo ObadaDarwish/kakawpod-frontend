c1a2ab5080b657e10bd7bde2509dd32cf766603e
Obada Darwish <obada_567@hotmail.co.uk>
2020-11-18 02:05:08 +0200

    feature: edit completed order in admin

--- src/components/Dialogs/AdminOrderViewDialog/AdminOrderViewDialog.js
+++ src/components/Dialogs/AdminOrderViewDialog/AdminOrderViewDialog.js
@@ -2,18 +2,24 @@ import React, { useRef, useState } from 'react';
 import DialogWrapper from '../DialogWrapper/DialogWrapper';
 import ButtonUI from '../../UI/ButtonUI/ButtonUI';
 import SelectUi from '../../UI/SelectUI/SelectUI';
+import EditIcon from '@material-ui/icons/Edit';
 import useCallServer from '../../../hooks/useCallServer';
 import {
     errorNotification,
+    infoNotification,
     successNotification,
 } from '../../../utils/notification-utils';
 import InputUI from '../../UI/InputUI/InputUI';
 import { useSelector } from 'react-redux';
-
+import { stringfyJSON } from '../../../utils/jsonConversion';
+import { useHistory } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 const AdminOrderViewDialog = ({ open, onClose, close, order }) => {
     const [status, setStatus] = useState(order.status);
     const isAuth = useSelector((state) => state.user);
-    const [showOTPButton, setshowOTPButton] = useState(true);
+    const history = useHistory();
+    const [showOTPButton, setShowOTPButton] = useState(true);
+    const [editCompletedOrder, setEditCompletedOrder] = useState(false);
     const [, , , , callServer, isLoading, setLoading] = useCallServer();
     const OTPRef = useRef();
     const statusChange = (e) => {
@@ -66,7 +72,7 @@ const AdminOrderViewDialog = ({ open, onClose, close, order }) => {
             percentage: 0,
         })
             .then(() => {
-                setshowOTPButton(false);
+                setShowOTPButton(false);
                 successNotification(
                     'OTP has been send successfully',
                     'Discount'
@@ -79,6 +85,61 @@ const AdminOrderViewDialog = ({ open, onClose, close, order }) => {
             })
             .finally(() => setLoading(false));
     };
+    const handleEditCompletedOrder = () => {
+        setEditCompletedOrder(true);
+        setShowOTPButton(false);
+        requestOTP();
+    };
+    const editOrder = () => {
+        let OTP = OTPRef.current.value;
+        const callEditOrder = (OTPValue) => {
+            callServer(
+                'PUT',
+                `${process.env.REACT_APP_API_ENDPOINT}/admin/completedOrder/${order._id}`,
+                { OTP: OTPValue }
+            )
+                .then((response) => {
+                    const getTotal = (items) => {
+                        let total = 0;
+                        items.forEach((item) => {
+                            total += item.count * item.price;
+                        });
+                        return total;
+                    };
+                    let total = getTotal(response.data.items);
+                    let updatedItems = response.data.items.map((item) => {
+                        return {
+                            ...item,
+                            total: item.price,
+                            uid: uuidv4(),
+                        };
+                    });
+                    localStorage.setItem(
+                        'pos',
+                        stringfyJSON({ items: updatedItems, total: total })
+                    );
+                    history.push('/admin/pos');
+                })
+                .catch((err) => {
+                    if (err.response) {
+                        setLoading(false);
+                        errorNotification(err.response.data.message, 'Order');
+                    }
+                });
+        };
+
+        if (isAuth.authority === 2) {
+            if (OTP) {
+                setLoading(true);
+                callEditOrder(OTP);
+            } else {
+                infoNotification('OTP is required', 'OTP');
+            }
+        } else {
+            setLoading(true);
+            callEditOrder(OTP);
+        }
+    };
     return (
         <DialogWrapper
             open={open}
@@ -87,6 +148,14 @@ const AdminOrderViewDialog = ({ open, onClose, close, order }) => {
             loading={isLoading}
         >
             <div className={'viewOrderContainer'}>
+                {order.status === 'completed' && (
+                    <EditIcon
+                        className={'viewOrderContainer__editIcon'}
+                        fontSize={'large'}
+                        onClick={handleEditCompletedOrder}
+                    />
+                )}
+
                 <section className={'viewOrderContainer__userSection'}>
                     <p> {order.user_id.name}</p>
                     <p> {order.user_id.phone}</p>
@@ -149,31 +218,38 @@ const AdminOrderViewDialog = ({ open, onClose, close, order }) => {
                     </table>
                 </section>
                 <section className={'viewOrderContainer__statusControl'}>
-                    <SelectUi
-                        label={'status'}
-                        value={status}
-                        handleChange={statusChange}
-                        list={[
-                            'pending',
-                            'out for delivery',
-                            'delivered',
-                            'canceled',
-                            'completed',
-                        ]}
-                    />
+                    {!editCompletedOrder && (
+                        <SelectUi
+                            label={'status'}
+                            value={status}
+                            handleChange={statusChange}
+                            list={[
+                                'pending',
+                                'out for delivery',
+                                'delivered',
+                                'canceled',
+                                'completed',
+                            ]}
+                        />
+                    )}
+
                     {canShowOTP() && !showOTPButton && (
                         <InputUI label={'OTP'} reference={OTPRef} />
                     )}
-                    {canShowOTP() && showOTPButton ? (
-                        <ButtonUI
-                            name={'request otp'}
-                            clickHandler={requestOTP}
-                        />
+                    {!editCompletedOrder ? (
+                        canShowOTP() && showOTPButton ? (
+                            <ButtonUI
+                                name={'request otp'}
+                                clickHandler={requestOTP}
+                            />
+                        ) : (
+                            <ButtonUI
+                                name={'save'}
+                                clickHandler={saveStatusChange}
+                            />
+                        )
                     ) : (
-                        <ButtonUI
-                            name={'save'}
-                            clickHandler={saveStatusChange}
-                        />
+                        <ButtonUI name={'edit'} clickHandler={editOrder} />
                     )}
                 </section>
             </div>
